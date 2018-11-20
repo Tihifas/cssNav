459ec466941c5ef25f8e7b4537297960566901e2
Torstein Bratberg (Kursus Lokale) <tdbratberg@gmail.com>
2018-11-20 15:08:26 +0100

    Add project files.

---
+++ ContosoConf/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ContosoConf
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteTable.Routes.MapRoute(
+                            "Default",
+                            "{controller}/{action}/{id}",
+                            new { id = UrlParameter.Optional }
+                        );
+        }
+    }
+}
